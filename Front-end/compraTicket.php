9b3113e4e27a7cd0ecc96247b552ccc78a96ccfa
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-22 15:01:00 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -202,6 +202,44 @@ function connection()
     </form>
   </section>
   <!-- End  Section1 -->
+  <!-- ======= Cta Section ======= -->
+  <section id="cta" class="cta">
+    <div class="container" data-aos="zoom-in">
+
+      <div class="text-center">
+        <form action="" method="post" role="form" class="php-email-form">
+          <div class="container">
+            <p>
+            <h4>Agendar viaje</h4>
+            <br>Seleccione la fecha en la que quiere viajar.
+            </p>
+
+            <div class="content">
+
+              <div class="container text-left">
+
+                <div class="row justify-content-center">
+                  <div class="col-md-10 text-center">
+
+                    <input type="text" class="form-control w-25 mx-auto mb-3" id="result" placeholder="Select date" disabled="">
+                    <form action="#" class="row">
+                      <div class="col-md-12">
+                        <div id="inline_cal"></div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div class=""><button type="submit">Seleccionar</button></div>
+
+          </div>
+        </form>
+      </div>
+
+    </div>
+  </section><!-- End Cta Section -->
   <!-- ======= Section1 ======= -->
   <section id="horarios" class="inner-page">
     <form action="" method="post" role="form" class="php-email-form">
@@ -302,35 +340,7 @@ function connection()
   </section>
   <!-- ======= Section2 ======= -->
   <section class="inner-page">
-    <form action="" method="post" role="form" class="php-email-form">
-      <div class="container">
-        <p>
-        <h4>Agendar viaje</h4>
-        <br>Seleccione la fecha en la que quiere viajar.
-        </p>
 
-        <div class="content">
-
-          <div class="container text-left">
-
-            <div class="row justify-content-center">
-              <div class="col-md-10 text-center">
-
-                <input type="text" class="form-control w-25 mx-auto mb-3" id="result" placeholder="Select date" disabled="">
-                <form action="#" class="row">
-                  <div class="col-md-12">
-                    <div id="inline_cal"></div>
-                  </div>
-                </form>
-              </div>
-            </div>
-
-          </div>
-        </div>
-        <div class=""><button type="submit">Seleccionar</button></div>
-
-      </div>
-    </form>
   </section>
 
   <!-- ======= Cta Section ======= -->
