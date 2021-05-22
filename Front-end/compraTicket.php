c690867edde4c5872366b56c430dcdef3f0bfc21
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-21 23:07:06 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -116,7 +116,7 @@ $idUsuario = $_SESSION['idUsuario'];
         <div class="d-flex justify-content-between align-items-center">
           <h2>Compra de tiquetes</h2>
           <ol>
-            <li><a href="index.html">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li>Compra de tiquetes</li>
           </ol>
         </div>
