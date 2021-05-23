c8ca8230c9b07f7e8e8518b4982d9aa9203ae44c
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-23 10:46:53 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -252,39 +252,18 @@ function genCod()
       <form id="regiration_form" novalidate action="action.php" method="post">
         <fieldset>
           <h2>Paso 1: Crear su cuenta</h2>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="data[email]" placeholder="Email">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Password">
-          </div>
+          
           <input type="button" name="data[password]" class="next btn btn-info" value="Siguiente" />
         </fieldset>
         <fieldset>
           <h2> Paso 2: Agregar detalles personales</h2>
-          <div class="form-group">
-            <label for="fName">Nombres</label>
-            <input type="text" class="form-control" name="data[fName]" id="fName" placeholder="Nombres">
-          </div>
-          <div class="form-group">
-            <label for="lName">Apellidos</label>
-            <input type="text" class="form-control" name="data[lName]" id="lName" placeholder="Apellidos">
-          </div>
+          
           <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
           <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
         </fieldset>
         <fieldset>
           <h2>Paso 3: Información de contacto</h2>
-          <div class="form-group">
-            <label for="mob">Numero Celular</label>
-            <input type="text" class="form-control" id="mob" name="data[mob]" placeholder="Numero Celular">
-          </div>
-          <div class="form-group">
-            <label for="address">Direccion</label>
-            <textarea class="form-control" name="data[address]" placeholder="Direccion"></textarea>
-          </div>
+          
           <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
           <input type="submit" name="submit" class="submit btn btn-success" value="Enviar" id="submit_data" />
         </fieldset>
