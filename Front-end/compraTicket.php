428c1582b4f6122f1f5d7d5d3d8b6e11f7cc1ae1
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-23 14:53:19 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -550,35 +550,42 @@ $fechaSalida = 'fechaSalida';
 </html>
 <script type="text/javascript">
   $(document).ready(function() {
-    var current = 1,
-      current_step, next_step, steps;
-    steps = $("fieldset").length;
-    $(".next").click(function() {
-      current_step = $(this).parent();
-      next_step = $(this).parent().next();
-      next_step.show();
-      current_step.hide();
-      setProgressBar(++current);
-    });
-    $(".previous").click(function() {
-      current_step = $(this).parent();
-      next_step = $(this).parent().prev();
-      next_step.show();
-      current_step.hide();
-      setProgressBar(--current);
-    });
-    setProgressBar(current);
-    // Change progress bar action
-    function setProgressBar(curStep) {
-      var percent = parseFloat(100 / steps) * curStep;
-      percent = percent.toFixed();
-      $(".progress-bar")
-        .css("width", percent + "%")
-        .html(percent + "%");
-    }
-    $(".payment-button").click(function() {
-      alert("holi");
-    });
+        var current = 1,
+          current_step, next_step, steps;
+        steps = $("fieldset").length;
+        $(".next").click(function() {
+          current_step = $(this).parent();
+          next_step = $(this).parent().next();
+          next_step.show();
+          current_step.hide();
+          setProgressBar(++current);
+        });
+        $(".previous").click(function() {
+          current_step = $(this).parent();
+          next_step = $(this).parent().prev();
+          next_step.show();
+          current_step.hide();
+          setProgressBar(--current);
+        });
+        setProgressBar(current);
+        // Change progress bar action
+        function setProgressBar(curStep) {
+          var percent = parseFloat(100 / steps) * curStep;
+          percent = percent.toFixed();
+          $(".progress-bar")
+            .css("width", percent + "%")
+            .html(percent + "%");
+        }
+        $(".payment-button").click(function() {
+          $.ajax({
+            type: 'POST',
+            url: '../Back-end/procesos/crearTarjetas.php',
+            success: function(data) {
+              alert(data);
+              $("p").text(data);
 
-  });
+            }
+          });
+
+        });
 </script>
