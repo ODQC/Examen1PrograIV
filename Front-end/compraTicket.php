062b143a29bb0bb339a81e0d91fbcd01e396c42d
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-23 09:08:05 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -245,18 +245,20 @@ function genCod()
   <section class="inner-page">
     <div class="container">
       <p>
-        Example inner page template
+      <h4>Forma de pago</h4>
+      <br>Ingrese el método de pago.
       </p>
 
       <div class="container">
-        <h1>Registro de usuarios paso a paso</h1>
+        <h1>Reserva de tiquetes</h1>
         <div class="progress">
           <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
-
+        <!-- ================================INICIO DEL FORM=========================================== -->
         <form id="regiration_form" novalidate action="action.php" method="post">
+          <!-- ================================PASO1=========================================== -->
           <fieldset>
-            <h2>Paso 1: Crear su cuenta</h2>
+            <h2>Paso 1: Seleccione la fecha</h2>
             <div class="form-group">
               <label for="email">Email</label>
               <input type="email" class="form-control" id="email" name="data[email]" placeholder="Email">
@@ -267,8 +269,11 @@ function genCod()
             </div>
             <input type="button" name="data[password]" class="next btn btn-info" value="Siguiente" />
           </fieldset>
+          <!-- ================================PASO2=========================================== -->
           <fieldset>
-            <h2> Paso 2: Agregar detalles personales</h2>
+
+
+            <h2> Paso 2: Seleccione el Horario</h2>
             <div class="form-group">
               <label for="fName">Nombres</label>
               <input type="text" class="form-control" name="data[fName]" id="fName" placeholder="Nombres">
@@ -280,7 +285,25 @@ function genCod()
             <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
             <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
           </fieldset>
+          <!-- ================================PASO3=========================================== -->
           <fieldset>
+
+
+            <h2> Paso 2: Seleccione el Espacio</h2>
+            <div class="form-group">
+              <label for="fName">Nombres</label>
+              <input type="text" class="form-control" name="data[fName]" id="fName" placeholder="Nombres">
+            </div>
+            <div class="form-group">
+              <label for="lName">Apellidos</label>
+              <input type="text" class="form-control" name="data[lName]" id="lName" placeholder="Apellidos">
+            </div>
+            <input type="button" name="previous" class="previous btn btn-default" value="Previo" />
+            <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
+          </fieldset>
+          <!-- ================================PASO4=========================================== -->
+          <fieldset>
+
             <h2>Paso 3: Información de contacto</h2>
             <div class="form-group">
               <label for="mob">Numero Celular</label>
