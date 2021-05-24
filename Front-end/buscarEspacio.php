2134ad945e6f6eea722a68e3d617f8df67271583
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-23 18:21:32 -0600

    Update buscarEspacio.php

--- Front-end/buscarEspacio.php
+++ Front-end/buscarEspacio.php
@@ -19,13 +19,15 @@
     if (isset($_POST['Buses_idBus'])) {
         $conn = connection();
         $Buses_idBus = $_POST['Buses_idBus'];
-                echo $Buses_idBus;
+                
         $sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE (Buses_idBus=$Buses_idBus AND estado='Disponible')";
         $result = mysqli_query($conn, $sql);
-      ?>
-        <option value="">--Selecionar--</option>
 
-            <?php while ($row1 = mysqli_fetch_array($result)) :; ?>
+    echo '<script type="text/JavaScript"> 
+			alert("Si llegué");
+		</script>';
+
+            while ($row1 = mysqli_fetch_array($result)) :; ?>
 
                 <option value=" <?php echo $row1['idEspacio']; ?>"><?php echo $row1['numAsiento']; ?></option>
 
