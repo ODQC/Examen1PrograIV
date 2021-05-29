c97b85faf0b64944591758611211e2551f0b718a
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-29 16:23:53 -0600

    test

--- Front-end/buscarEspacio.php
+++ Front-end/buscarEspacio.php
@@ -15,12 +15,21 @@ function connection()
 }
 function buscar($idHorario){
     $conn = connection();
-    $sql = "SELECT * FROM `HorariosBus`.`Horarios`";
-    $result = $conn->query($sql);
-    
+    $sql =
+    "SELECT * FROM `HorariosBus`.`Horarios` WHERE idhorario = '" . $idHorario . "'";;
+    $result = mysqli_query($conn, $sql);
+   
+   if( $row1 = mysqli_fetch_array($result)){
+    echo '<script type="text/JavaScript"> 
+			alert("Se ha encotrado registro");
+		</script>';
 
-
-
+    return $row1['Buses_idBus'];
+   }else{
+		echo '<script type="text/JavaScript"> 
+			alert("No hemos encotrado ningun registro");
+		</script>';
+	}
 
 }
 
@@ -42,16 +51,17 @@ function buscar($idHorario){
     }
 
     $q = intval($_GET['q']);   
-
-    mysqli_select_db($conn, "ajax_demo");
-    $sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= '".$q."'";
-    $result = mysqli_query($conn, $sql);
-    ?>
+    buscar($q);
+    #mysqli_select_db($conn, "ajax_demo");
+    #$sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= '".$q."'";
+    #$result = mysqli_query($conn, $sql);
+    #?>
         <option value="0">--Selecionar--</option>
     <?php
 
-    while ($row = mysqli_fetch_array($result)) {?>
-    <option value=" <?php echo $row["idEspacio"]; ?>"><?php echo ($row["numAsiento"]); ?></option>';
-    <?php }
+    #while ($row = mysqli_fetch_array($result)) {?>
+    
+    <option value=" <?php #echo $row["idEspacio"]; ?>"><?php #echo ($row["numAsiento"]); ?></option>';
+    <?php #}
 
      mysqli_close($con);?>
