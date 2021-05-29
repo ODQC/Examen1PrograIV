1d2e432630f6242eb057bf96677a09fd2b1d8402
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-28 21:49:23 -0600

    Update compraTicket.php

--- Front-end/compraTicket.php
+++ Front-end/compraTicket.php
@@ -549,7 +549,7 @@ $fechaEmision = date('Y-m-d H:i:s');
 
     try {
       var ced = <?php echo $idUsuario ?>;
-      var idTicket = Math.random().toString(36).substring(16);;
+      var idTicket = crypto.randomBytes(20).toString('hex');
       //var emision =<?php # echo $fechaEmision 
                       ?>;
       var espacio = document.getElementById("idEspacio").value;
