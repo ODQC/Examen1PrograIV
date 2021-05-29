79d32bd94933f774c82500de9c626b876f74d88e
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-28 20:28:54 -0600

    Update buscarEspacio.php

--- Front-end/buscarEspacio.php
+++ Front-end/buscarEspacio.php
@@ -2,37 +2,28 @@
 
 
 
-$servername = "localhost";
-$username = "root";
-$password = "207460988";
-$dbname = "HorariosBus";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$q = intval($_GET['q']);
+    $servername = "localhost";
+    $username = "root";
+    $password = "207460988";
+    $dbname = "HorariosBus";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$conn = connection();
-$sql = "SELECT * FROM `HorariosBus`.`Horarios` WHERE idhorario ='" . $q . "'";
-$result = mysqli_query($conn, $sql);
+    $q = intval($_GET['q']);   
 
+    mysqli_select_db($conn, "ajax_demo");
+    $sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= '".$q."'";
+    $result = mysqli_query($conn, $sql);
+    ?>
+        <option value="0">--Selecionar--</option>
+    <?php
 
-    $row = mysqli_fetch_array($result);
-    $idBus = $row["Buses_idBus"];
+    while ($row = mysqli_fetch_array($result)) {?>
+    <option value=" <?php echo $row["idEspacio"]; ?>"><?php echo ($row["numAsiento"]); ?></option>';
+    <?php }
 
-
-
-mysqli_select_db($conn, "ajax_demo");
-$sql1 = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= $idBus" ;
-$result1 = mysqli_query($conn, $sql1);
-?>
-<option value="0">--Selecionar--</option>
-<?php
-
-while ($row1 = mysqli_fetch_array($result1)) { ?>
-    <option value=" <?php echo $row1["idEspacio"]; ?>"><?php echo ($row1["numAsiento"]); ?></option>';
-<?php }
-
-mysqli_close($con); ?>
+     mysqli_close($con);?>
