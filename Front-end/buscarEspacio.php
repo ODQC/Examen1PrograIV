28ae1f2edd1ff7126bddad2778b619d162630296
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-29 16:28:54 -0600

    Update buscarEspacio.php

--- Front-end/buscarEspacio.php
+++ Front-end/buscarEspacio.php
@@ -1,6 +1,7 @@
 <?php
-function connection()
-{
+
+
+
     $servername = "localhost";
     $username = "root";
     $password = "207460988";
@@ -11,57 +12,18 @@ function connection()
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    return $conn;
-}
-function buscar($idHorario){
-    $conn = connection();
-    $sql =
-    "SELECT * FROM `HorariosBus`.`Horarios` WHERE idhorario = '" . $idHorario . "'";;
-    $result = mysqli_query($conn, $sql);
-   
-   if( $row1 = mysqli_fetch_array($result)){
-    echo '<script type="text/JavaScript"> 
-			alert("Se ha encotrado registro");
-		</script>';
-
-    return $row1['Buses_idBus'];
-   }else{
-		echo '<script type="text/JavaScript"> 
-			alert("No hemos encotrado ningun registro");
-		</script>';
-	}
-
-}
-
-
-
-?>
-
-
-<?php
-
-
-
-   
-    // Create connection
-    $conn = connection();
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
     $q = intval($_GET['q']);   
-    buscar($q);
-    #mysqli_select_db($conn, "ajax_demo");
-    #$sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= '".$q."'";
-    #$result = mysqli_query($conn, $sql);
-    #?>
+
+    mysqli_select_db($conn, "ajax_demo");
+    $sql = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= '".$q."'";
+    $result = mysqli_query($conn, $sql);
+    ?>
         <option value="0">--Selecionar--</option>
     <?php
 
-    #while ($row = mysqli_fetch_array($result)) {?>
-    
-    <option value=" <?php #echo $row["idEspacio"]; ?>"><?php #echo ($row["numAsiento"]); ?></option>';
-    <?php #}
+    while ($row = mysqli_fetch_array($result)) {?>
+    <option value=" <?php echo $row["idEspacio"]; ?>"><?php echo ($row["numAsiento"]); ?></option>';
+    <?php }
 
      mysqli_close($con);?>
