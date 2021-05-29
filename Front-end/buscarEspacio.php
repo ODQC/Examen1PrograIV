6340fa3a7499bf9e64977c4e2e939cd5d607e590
Oscar Quesada <oscaardanielqc@outlook.es>
2021-05-28 20:09:08 -0600

    Update buscarEspacio.php

--- Front-end/buscarEspacio.php
+++ Front-end/buscarEspacio.php
@@ -18,11 +18,11 @@ $conn = connection();
 $sql = "SELECT * FROM `HorariosBus`.`Horarios` WHERE idhorario ='" . $q . "'";
 $result = mysqli_query($conn, $sql);
 
-if ($result->num_rows > 0) {
+
     $row = $result->fetch_assoc();
     $idBus = $row["Buses_idBus"];
 
-}
+
 
 mysqli_select_db($conn, "ajax_demo");
 $sql1 = "SELECT * FROM `HorariosBus`.`Espacios` WHERE estado='Disponible' AND Buses_idBus= $idBus" ;
